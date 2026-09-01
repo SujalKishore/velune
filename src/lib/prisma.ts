ed09dc7ff7ad0cbd00f4430d5ccfd77ba3d6ff64
SujalKishore <131854159+SujalKishore@users.noreply.github.com>
2026-09-01 22:46:58 +0530

    update more

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,14 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
 
 const globalForPrisma = globalThis as unknown as { prisma_v6: PrismaClient };
 
-export const prisma = globalForPrisma.prisma_v6 ?? new PrismaClient();
+export const prisma = globalForPrisma.prisma_v6 ?? new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma_v6 = prisma;
