4def5f161e8413e04af554ab2801de6450f7b4fa
SujalKishore <131854159+SujalKishore@users.noreply.github.com>
2026-09-01 22:40:09 +0530

    Remove SQLite adapter

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,8 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaLibSql } from "@prisma/adapter-libsql";
 
 const globalForPrisma = globalThis as unknown as { prisma_v6: PrismaClient };
 
-export const prisma = globalForPrisma.prisma_v6 ?? new PrismaClient({ adapter: new PrismaLibSql({ url: "file:./dev.db" }) });
+export const prisma = globalForPrisma.prisma_v6 ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma_v6 = prisma;
